c90a0964a45506bb51e5d8214b968ed692a5a42d
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-05 16:30:41 -0500

    Initial check-in

---
+++ setup.py
@@ -0,0 +1,71 @@
+import codecs
+from setuptools import setup
+from setuptools import find_packages
+
+entry_points = {
+    'console_scripts': [
+    ],
+}
+
+TESTS_REQUIRE = [
+    'nti.app.testing',
+    'zope.dottedname',
+    'zope.testrunner',
+]
+
+
+def _read(fname):
+    with codecs.open(fname, encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name='nti.app.xapi',
+    version=_read('version.txt').strip(),
+    author='Chris Utz',
+    author_email='chris@nextthought.com',
+    description="NTI xAPI Pyramid layer",
+    long_description=(
+        _read('README.rst')
+        + '\n\n'
+        + _read("CHANGES.rst")
+    ),
+    license='Apache',
+    keywords='xAPI',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    zip_safe=True,
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    namespace_packages=['nti', 'nti.app'],
+    tests_require=TESTS_REQUIRE,
+    install_requires=[
+        'setuptools',
+        'nti.asynchronous',
+        'nti.xapi',
+        'pyramid',
+        'zope.component',
+        'zope.i18nmessageid',
+        'zope.interface',
+        'zope.security',
+    ],
+    extras_require={
+        'test': TESTS_REQUIRE,
+        'docs': [
+            'Sphinx',
+            'repoze.sphinx.autointerface',
+            'sphinx_rtd_theme',
+        ],
+    },
+    entry_points=entry_points,
+)
