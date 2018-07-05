6efa4c92986d6cf64002bb5e0838c00f2354c517
Carlos Sanchez <carlos.sanchez@nextthought.com>
2018-07-05 18:24:15 -0500

    add factory func

--- src/nti/app/xapi/__init__.py
+++ src/nti/app/xapi/__init__.py
@@ -10,3 +10,11 @@ from __future__ import absolute_import
 
 import zope.i18nmessageid
 MessageFactory = zope.i18nmessageid.MessageFactory(__name__)
+
+from zope import component
+
+from nti.app.xapi.interfaces import IXAPIJobQueueFactory
+
+
+def get_factory():
+    return component.getUtility(IXAPIJobQueueFactory)
