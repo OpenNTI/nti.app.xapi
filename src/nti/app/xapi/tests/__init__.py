58adcf248150c64794b0812accd313341bfef240
cutz <christopher.m.utz@gmail.com>
2018-07-09 11:13:22 -0500

    missed a bunch of files

--- src/nti/app/xapi/tests/__init__.py
+++ src/nti/app/xapi/tests/__init__.py
@@ -5,4 +5,38 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import absolute_import
 
-# pylint: disable=protected-access,too-many-public-methods,arguments-differ
+# disable: accessing protected members, too many methods
+# pylint: disable=W0212,R0904
+
+import zope.testing.cleanup
+
+from nti.dataserver.tests.mock_dataserver import DSInjectorMixin
+
+from nti.testing.layers import GCLayerMixin
+from nti.testing.layers import ZopeComponentLayer
+from nti.testing.layers import ConfiguringLayerMixin
+
+
+class SharedConfiguringTestLayer(ZopeComponentLayer,
+                                 GCLayerMixin,
+                                 ConfiguringLayerMixin,
+                                 DSInjectorMixin):
+
+    set_up_packages = ('nti.dataserver', 'nti.app.xapi')
+
+    @classmethod
+    def setUp(cls):
+        cls.setUpPackages()
+
+    @classmethod
+    def tearDown(cls):
+        cls.tearDownPackages()
+        zope.testing.cleanup.cleanUp()
+
+    @classmethod
+    def testSetUp(cls, test=None):
+        cls.setUpTestDS(test)
+
+    @classmethod
+    def testTearDown(cls):
+        pass
