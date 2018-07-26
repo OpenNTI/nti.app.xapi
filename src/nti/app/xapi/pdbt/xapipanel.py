ec97a32f5eada060e8ef6e81d200b05818000eba
cutz <christopher.m.utz@gmail.com>
2018-07-25 20:43:29 -0500

    Add a pyramid debug toolbar panel that shows statements generated during a request.

---
+++ src/nti/app/xapi/pdbt/xapipanel.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+.. $Id$
+"""
+
+from __future__ import division
+from __future__ import print_function
+from __future__ import absolute_import
+
+from pyramid_debugtoolbar.panels import DebugPanel
+
+from pyramid.threadlocal import get_current_request
+
+from zope import component
+
+from zope.component import getGlobalSiteManager
+
+from nti.app.xapi.interfaces import IStatementRecordedEvent
+
+from nti.xapi.interfaces import IStatement
+
+@component.adapter(IStatement, IStatementRecordedEvent)
+def _on_statement_recorded(stmt, event):
+    request = get_current_request()
+    if hasattr(request, '_nti_pdbt_xapi_statements'):
+        request._nti_pdbt_xapi_statements.append(stmt)
+
+
+gsm = getGlobalSiteManager()
+gsm.registerHandler(_on_statement_recorded)
+del gsm
+
+class XAPIDebugPanel(DebugPanel):
+    """
+    A debug panel capturing xapi statements that are recorded
+    during the request
+    """
+    name = 'xAPI'
+    template = 'nti.app.xapi:pdbt/templates/sample.dbtmako'
+
+    def __init__(self, request):
+        self.statements = request._nti_pdbt_xapi_statements = []
+
+    @property
+    def nav_title(self):
+        return self.name
+
+    @property
+    def title(self):
+        return self.name
+
+    @property
+    def has_content(self):
+        return bool(self.statements)
+
+    @property
+    def nav_subtitle(self):
+        if self.statements:
+            return "%d" % (len(self.statements))
+
+    def process_response(self, response):
+        stmts = sorted(self.statements, key=lambda stmt: stmt.timestamp)
+        self.data = {'statements': stmts}
+        
+
+def includeme(config):
+    config.add_debugtoolbar_panel(XAPIDebugPanel)
